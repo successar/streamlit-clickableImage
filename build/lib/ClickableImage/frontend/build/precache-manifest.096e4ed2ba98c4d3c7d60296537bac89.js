d4c7ccfaca9942f228af7d6ea05164d7878f5146
Sarthak Jain <sarjain@adobe.com>
2021-07-22 14:55:53 -0400

    Initial Commit

---
+++ build/lib/ClickableImage/frontend/build/precache-manifest.096e4ed2ba98c4d3c7d60296537bac89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad229bcdcffa47e114e2f476cde64cb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "02589bbd8ecf209c4876",
+    "url": "./static/js/2.44ffda57.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.44ffda57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e5512fa9de4a9cfc21",
+    "url": "./static/js/main.a4ca5e7b.chunk.js"
+  },
+  {
+    "revision": "6f41c30f8a03ef23fc57",
+    "url": "./static/js/runtime-main.e6d57ae0.js"
+  }
+]);
